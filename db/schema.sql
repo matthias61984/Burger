4ed9c6784e0410f70d197399b09f8a56ab72ac06
Corey Walsh <matthias61984@hotmail.com>
2018-11-19 18:51:13 -0800

    incomplete

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,10 @@
 DROP DATABASE IF EXISTS burgers_db;
 CREATE database burgers_db;
-
 USE burgers_db;
 
 CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT,
   burger_name VARCHAR(100) NOT NULL,
-  devoured BOOLEAN,
+  devoured BOOLEAN default false,
   PRIMARY KEY (id)
 );
